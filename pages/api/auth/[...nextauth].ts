fee44e4e55f17eed86c5ba6e6026d65777c8f1f0
Supanut1911 <ynotmela@gmail.com>
2023-06-27 09:41:16 +0700

    edit config provider

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -11,9 +11,10 @@ export const authOptions: NextAuthOptions = {
       clientId: process.env.COGNITO_CLIENT_ID,
       clientSecret: process.env.COGNITO_CLIENT_SECRET,
       issuer: process.env.COGNITO_ISSUER,
-      checks: ["state", "pkce"],
+      checks: ["state", "pkce", "nonce"],
     }),
   ],
+
   // pages: {
   //   // signIn:
   //   //   "https://my-super-app-poc-authentication-page.auth.ap-southeast-1.amazoncognito.com/login?client_id=24do2d0hn8rqmc5b9c41bn9g2a&scope=openid&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fapi%2Fauth%2Fcallback%2Fcognito&state=0UhPboKmLlPyQyPK0Z2UJYdyZvQiWsIU3ESEQI0CJ8A&code_challenge=fhDtxEz5zf4LDLtJ0ZU240d3fGKF2YCfRCRR1jBFvPE&code_challenge_method=S256",
@@ -33,6 +34,7 @@ export const authOptions: NextAuthOptions = {
       if (account) {
         token.accessToken = account.access_token;
         token.idToken = account.id_token;
+        token.refreshToken = account.refresh_token;
       }
 
       return token;
